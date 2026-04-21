5bcec090319fbc9f73baa435bc0385ed547ce30d
Fernando Estrada <fernaest213@gmail.com>
2026-04-20 19:39:53 -0600

    general struct for ekf

--- src/puzzlebot_control/src/odometry.cpp
+++ src/puzzlebot_control/src/odometry.cpp
@@ -30,6 +30,7 @@ which is:
 #include <nav_msgs/msg/odometry.hpp>
 #include <std_msgs/msg/float32.hpp>
 #include <cmath>
+#include <kalman_filter.hpp>
 
 class OdometryNode : public rclcpp::Node{
     public:
@@ -40,22 +41,31 @@ class OdometryNode : public rclcpp::Node{
             last_time_(this->get_clock()->now()){
 
 
-        odom_pub_ = this-> create_publisher<nav_msgs::msg::Odometry>("/odom", 10);
-
         encl_sub_ = this -> create_subscription<std_msgs::msg::Float32>("/VelEncL", 10,
             std::bind(&OdometryNode::encoderL_callback, this, std::placeholders::_1));
 
         encr_sub_ = this -> create_subscription<std_msgs::msg::Float32>("/VelEncR", 10, 
         std::bind(&OdometryNode::encoderR_callback, this, std::placeholders::_1));
 
+
+        aruco_sub = this -> create_subscription<std_msgs::msg::Float32>("/VelEncR", 10, 
+        std::bind(&OdometryNode::encoderR_callback, this, std::placeholders::_1));
+
+
+
         timer_ = this ->create_wall_timer(std::chrono::milliseconds(50), 
                 std::bind(&OdometryNode::publish_odometry, this)); 
+
+        odom_pub_ = this-> create_publisher<nav_msgs::msg::Odometry>("/odom", 10);
+
+        kalman_ = std::make_unique<ExtendedKalmanFilter>(r_, L_);
 
 
         RCLCPP_INFO(this->get_logger(), "Reading encoder velocities");
         
     }
     private:
+
         void encoderL_callback(const std_msgs::msg::Float32::SharedPtr msg){
             wheel_vel_left_rads_ = msg -> data; 
         }
@@ -63,6 +73,29 @@ class OdometryNode : public rclcpp::Node{
         void encoderR_callback(const std_msgs::msg::Float32::SharedPtr msg){
             wheel_vel_right_rads_ = msg -> data; 
         }
+
+        void aruco_callback(const aruco_msgs::msg::MarkerArray::SharedPtr msg){
+
+            for (const auto& marker :msg-> markers){
+                auto iterator = landmark_map_.find(marker.id) //find() looks for the key in the map and returns where it is
+                if (iterator == landmark_map.end()){ // if not, iterator is end() == NULL
+                    continue; 
+                auto landmark = it ->second; //landmark has the value if found
+
+                
+
+                }
+                
+            }
+
+            }
+
+            
+
+
+        }
+
+
 
         void get_odom(){
             rclcpp::Time now = this ->get_clock()->now(); 
@@ -73,37 +106,30 @@ class OdometryNode : public rclcpp::Node{
             double vL = wheel_vel_left_rads_ * r_; //lineal velocity for each wheel
             double vR = wheel_vel_right_rads_ * r_; 
 
-            double v_robot = (vR + vL) / 2.0;  //lineal velocity for the robot
-            double w_robot = (vR - vL) / L_;  //angular velocity for the whole robot
+            //double v_robot = (vR + vL) / 2.0;  //lineal velocity for the robot (part of twist message)
+            //double w_robot = (vR - vL) / L_;  //angular velocity for the whole robot (part of twist message)
             
-            double d_translation = v_robot * dt; //distance instead of velocity
-            double d_rot = w_robot * dt; //angular distance instead of velocity
+            //double d_translation = v_robot * dt; //distance instead of velocity
+            //double d_rot = w_robot * dt; //angular distance instead of velocity
 
-            x_ += d_translation * std::cos(theta_ + d_rot / 2.0); // x pos of the robot
-            y_ += d_translation * std::sin(theta_ + d_rot / 2.0);  // y pos of the robot
-            theta_ += d_rot; //orientation
 
-            theta_ = std::atan2(std::sin(theta_), std::cos(theta_)); 
+            //x_ += d_translation * std::cos(theta_ + d_rot / 2.0); // x pos of the robot
+            //y_ += d_translation * std::sin(theta_ + d_rot / 2.0);  // y pos of the robot
+            //theta_ += d_rot; //orientation
+            //theta_ = std::atan2(std::sin(theta_), std::cos(theta_)); 
+
+            kalman_.predict(vL, vR, wheel_vel_left_rads_, wheel_vel_right_rads_, dt) //call kalman prediction uses lineal vel and angular vel
 
         }
+
+        void 
+
+
+
 
 
         void publish_odometry(){
             get_odom(); 
-
-            nav_msgs::msg::Odometry msg; 
-            msg.header.stamp = this -> get_clock() -> now(); 
-            msg.header.frame_id = "odom"; 
-            msg.child_frame_id = "base_link"; 
-
-            msg.pose.pose.position.x = x_; 
-            msg.pose.pose.position.y = y_; 
-            msg.pose.pose.orientation.w = std::cos(theta_ / 2.0);
-            msg.pose.pose.orientation.z = std::sin(theta_ / 2.0);
-
-            msg.twist.twist.linear.x  = (wheel_vel_left_rads_ + wheel_vel_right_rads_) / 2.0 * r_;
-            msg.twist.twist.angular.z = (wheel_vel_right_rads_ - wheel_vel_left_rads_) / L_ * r_;
-
             odom_pub_ -> publish(msg); 
         }
 
@@ -113,9 +139,11 @@ class OdometryNode : public rclcpp::Node{
         rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr encr_sub_; 
         rclcpp::Time last_time_; 
         rclcpp::TimerBase::SharedPtr timer_;
+        std::unique_ptr<ExtendedKalmanFilter> kalman_; 
 
         const double r_, L_;
         double wheel_vel_left_rads_, wheel_vel_right_rads_, x_, y_, theta_; 
+        aruco_msgs::msg::MarkerArray last_aruco_; 
         
 }; 
 
