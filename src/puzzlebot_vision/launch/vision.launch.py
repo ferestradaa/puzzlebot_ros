43a98b585b74299f74345bd273bbf1b1e56e89f0
Fernando Estrada <fernaest213@gmail.com>
2026-04-21 23:06:58 -0600

    convert submodules to regular directories

---
+++ src/puzzlebot_vision/launch/vision.launch.py
@@ -0,0 +1,38 @@
+import os 
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription 
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node 
+from launch.conditions import UnlessCondition
+
+
+def generate_launch_description():
+    
+    use_sim = LaunchConfiguration('use_sim')
+
+    sim_arg = DeclareLaunchArgument(
+        'use_sim', 
+        default_value='true'
+    )
+
+    apriltag_config = os.path.join(
+        get_package_share_directory('puzzlebot_vision'),
+        'cfg',
+        'tags_36h11.yaml'
+    )
+
+    apriltag_node = Node(   
+        package='puzzlebot_vision',
+        executable='apriltag_node',
+        remappings=[
+            ('image_rect', '/camera/image_raw'),
+            ('camera_info', '/camera/camera_info'),
+        ],
+        parameters=[apriltag_config],
+    )
+
+    return LaunchDescription([
+        sim_arg,
+        apriltag_node
+    ])
